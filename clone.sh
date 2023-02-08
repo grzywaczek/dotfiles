6611d533c0f000745a42b021d7b1a5cf0f73d957
Anna Grzywacz <annagrzywacz@interia.eu>
2023-02-08 09:53:11 +0100

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -10,8 +10,3 @@ git clone git@gitlab.com:Evionica_DEV/ata/ata-frontend-application.git $EVIONICA
 git clone git@gitlab.com:Evionica_DEV/ata/docs.git $EVIONICA/docs
 git clone git@gitlab.com:Evionica_DEV/ata/ata-cloud-deployment.git ata-cloud
 git clone git@gitlab.com:Evionica_DEV/ata/ata-core.git ata-core
-
-
-# Grixu
-git clone git@github.com:grixu/grixu.git $GRIXU/grixu
-git clone git@github.com:grixu/flashcards-old.git $GRIXU/flashcards-old
